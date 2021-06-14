6b5a4a311e62b37abec3b57ba092a933b2118e48
Caro407 <caro.lefevre407@hotmail.fr>
2021-06-14 18:07:30 +0200

    add login page

---
+++ pages/login.js
@@ -0,0 +1,35 @@
+const Login = () => {
+  const submitInfo = (event) => {
+    event.preventDefault();
+
+    const myBody = new FormData(event.target);
+    const myHeaders = new Headers();
+
+    fetch("http://localhost:3000/users/sign_in.json", {
+      method: "POST",
+      headers: myHeaders,
+      body: myBody
+    })
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={submitInfo}>
+        <label htmlFor="email">
+        E-mail
+        </label>
+        <input name="user[email]" id="email" type="text" placeholder="E-mail"></input>
+        <label htmlFor="password">
+        Mot de passe
+        </label>
+        <input name="user[password]" id="password" type="password" placeholder="******************"></input>
+        <button type="submit">
+        Se connecter
+        </button>
+      </form>
+    </div>
+  )
+};
+
+export default Login;
