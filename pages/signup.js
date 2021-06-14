986bfdf6b824d94ecf543ba9dce2fe0c9a0f3b4c
Caro407 <caro.lefevre407@hotmail.fr>
2021-06-14 17:59:56 +0200

    add signup page

---
+++ pages/signup.js
@@ -0,0 +1,35 @@
+const Signup = () => {
+  const submitInfo = (event) => {
+    event.preventDefault();
+
+    const myBody = new FormData(event.target);
+    const myHeaders = new Headers();
+
+    fetch("http://localhost:3000/users.json", {
+      method: "POST",
+      headers: myHeaders,
+      body: myBody
+    })
+  };
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={submitInfo}>
+      <label htmlFor="email">
+      E-mail
+      </label>
+      <input name="user[email]" id="email" type="text" placeholder="E-mail"></input>
+      <label htmlFor="password">
+      Mot de passe (6 caractères minimum)
+      </label>
+      <input name="user[password]" id="password" type="password" placeholder="******************"></input>
+      <button type="submit">
+      S'inscrire
+      </button>
+      </form>
+    </div>
+  )
+};
+
+export default Signup;
