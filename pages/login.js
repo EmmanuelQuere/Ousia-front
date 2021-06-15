a8e96519bb1d66411735b271ddd9531df31c26c1
Caro407 <caro.lefevre407@hotmail.fr>
2021-06-15 10:19:12 +0200

    linked login page to redux

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,11 @@
+import { useDispatch } from 'react-redux';
+import Cookies from 'js-cookie';
+import Router from 'next/router';
+import { logIn } from '../redux/actions/userActions';
+
 const Login = () => {
+  const dispatch = useDispatch();
+
   const submitInfo = (event) => {
     event.preventDefault();
 
@@ -10,6 +17,17 @@ const Login = () => {
       headers: myHeaders,
       body: myBody
     })
+      .then(response => {
+        if (response.headers.get('Authorization'))
+        {
+          Cookies.set('token', response.headers.get('Authorization'), { sameSite: 'lax' });
+          Cookies.set('isLoggedIn', true, { sameSite: 'lax' });
+          dispatch(logIn(Cookies.get('token')));
+          Router.push('/');
+        }
+        else (alert('Something went wrong'))
+      })
+      .catch(error => console.log('error', error));
   };
 
   return (
