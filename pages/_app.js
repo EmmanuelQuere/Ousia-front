7c5269e1d53452f2afcc8efb39b7b23327ea6ee5
Erwann Le Noac'h <erwann.lenoach@hotmail.fr>
2021-06-15 11:52:15 +0200

    footer

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import store from '../redux/store';
 import { Provider } from 'react-redux';
 import '../styles/globals.css';
 import Head from "next/head";
+import Footer from "../components/footer"
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -12,6 +13,7 @@ function MyApp({ Component, pageProps }) {
   <Provider store={store}>
     <Component {...pageProps} />
   </Provider>
+  <Footer/>
 
   </>
   );
