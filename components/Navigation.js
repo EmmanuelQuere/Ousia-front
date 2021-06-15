644cfb884abf49c84c46480503416ac1220bac45
Caro407 <caro.lefevre407@hotmail.fr>
2021-06-15 11:40:00 +0200

    update links on navbar

--- components/Navigation.js
+++ components/Navigation.js
@@ -4,16 +4,16 @@ import Link from 'next/link'
 const Navigation = () => {
 
   return (
-    <nav className='flex items-center justify-between bg-pink-500 p-4'>
-      <div className='flex items-center flex-shrink-0 text-white'><a className='text-lg tracking-wide font-medium uppercase pl-2' href="/">Ousia</a></div>
+    <nav className='flex items-center justify-between bg-gray-50 p-4'>
+      <div className='flex items-center flex-shrink-0 text-white'><a className='text-black font-bold text-lg tracking-wide font-medium uppercase pl-2' href="/">Ousia</a></div>
       <ul className='inline-flex w-full lg:flex lg:items-center lg:w-auto'>
-        <li className=""><a href="#">Magasin</a></li>
-        <li className=""><a href="#">La marque</a></li>
-        <li className=""><a href="#">La Team</a></li>
+        <Link href="/shop"><li className="mx-1"><a>Magasin</a></li></Link>
+        <li className="mx-1"><a href="#">La marque</a></li>
+        <li className="mx-1"><a href="#">La Team</a></li>
       </ul>
       <ul className='inline-flex lg:flex lg:items-center lg:w-auto'>
-        <li><a href="#">Se connecter</a></li>
-        <li><a href="#">S'inscrire</a></li>
+        <Link href="/login"><li className="mx-1"><a>Se connecter</a></li></Link>
+        <Link href="/signup"><li className="mx-1"><a>S'inscrire</a></li></Link>
         <DisconnectButton />
       </ul>
     </nav>
