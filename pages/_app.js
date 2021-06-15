8e2551c28cdd67a16c1965e0384a73cb1b8b223c
Ariane <pikabooariane@hotmail.fr>
2021-06-15 12:20:50 +0300

    moved tailwind utilities for faster compilation

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import store from '../redux/store';
 import { Provider } from 'react-redux';
 import '../styles/globals.css';
+import '../styles/tailwind_utilities.css';
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
