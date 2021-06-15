144fa854db34059f27680a562204bdb4da3aebdc
EmmanuelQuere <emmanuel.quere29@gmail.com>
2021-06-15 11:12:27 +0200

    tw navbar

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,12 +1,14 @@
 const Navigation = () => {
 
   return (
-    <nav className='navbar'>
-      <div className='brand'><a href="/">Ousia</a></div>
-      <ul>
-        <li className="leftnav"><a href="#">Magasin</a></li>
-        <li className="leftnav"><a href="#">La marque</a></li>
-        <li className="leftnav"><a href="#">La Team</a></li>
+    <nav className='flex items-center justify-between bg-pink-500 p-4'>
+      <div className='flex items-center flex-shrink-0 text-white'><a className='text-lg tracking-wide font-medium uppercase pl-2' href="/">Ousia</a></div>
+      <ul className='inline-flex w-full lg:flex lg:items-center lg:w-auto'>
+        <li className=""><a href="#">Magasin</a></li>
+        <li className=""><a href="#">La marque</a></li>
+        <li className=""><a href="#">La Team</a></li>
+      </ul>
+      <ul className='inline-flex lg:flex lg:items-center lg:w-auto'>
         <li><a href="#">Se connecter</a></li>
         <li><a href="#">S'inscrire</a></li>
         <li><a href="#">Se déconnecter</a></li>
