ef38420c8e36661c7a9dede427aca48472fef1e3
Ariane <pikabooariane@hotmail.fr>
2021-06-16 14:32:40 +0300

    began cart page

---
+++ pages/cart/index.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import CartProduct from '../../components/CartProduct';
+
+// export const getServerSideProps = async (context) => {
+//   const res = await fetch(`${process.env.url}/items.json`);
+//   const data = await res.json();
+//   return {
+//       props: {items: data}
+//   }
+// }
+
+const Cart = () => {
+  const [currentUserCart, setCurrentUserCart] = React.useState();
+  const userToken = useSelector(state => state.token);
+
+  React.useEffect(
+    () => {
+      if (userToken)
+      {let myHeaders = new Headers();
+      myHeaders.append("Authorization", `${userToken}`);
+
+      let requestOptions = {
+        method: 'GET',
+        headers: myHeaders
+      };
+
+      fetch(`${process.env.url}/mycart`, requestOptions)
+        .then(response => response.json())
+        .then(response => console.log(response))
+      .then(result=> setCurrentUserCart(result))
+      .catch(error => console.log('error', error));}
+    }
+    , []
+  )
+  
+  return (
+  <div className="container mx-auto mt-10">
+    <div className="flex shadow-md my-10">
+      <div className="w-3/4 bg-white px-10 py-10">
+        <div className="flex justify-between border-b pb-8">
+          <h1 className="font-semibold text-2xl">Mon panier</h1>
+          <h2 className="font-semibold text-2xl">3 Items</h2>
+        </div>
+        <div className="flex mt-10 mb-5">
+          <h3 className="font-semibold text-gray-600 text-xs uppercase w-2/5">Produit</h3>
+          <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Quantité</h3>
+          <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Prix</h3>
+          <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Total</h3>
+          </div>
+          {currentUserCart ? <span>"hello"</span> : <span>Pas de cart ici</span>}
+          <a href="#" className="flex font-semibold text-indigo-600 text-sm mt-10">
+      
+          <svg className="fill-current mr-2 text-indigo-600 w-4" viewBox="0 0 448 512"><path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z"/></svg>
+          Retour à la boutique
+        </a>
+      </div>
+
+      <div id="summary" className="w-1/4 px-8 py-10">
+        <h1 className="font-semibold text-2xl border-b pb-8">Récapitulatif de la commande</h1>
+        <div className="flex justify-between mt-10 mb-5">
+          <span className="font-semibold text-sm uppercase">3 articles</span>
+          <span className="font-semibold text-sm">590$</span>
+        </div>
+        <div>
+          <label className="font-medium inline-block mb-3 text-sm uppercase">Livraison</label>
+          <select className="block p-2 text-gray-600 w-full text-sm">
+            <option>Livraison standard - 10.00€</option>
+          </select>
+        </div>
+        <div className="py-10">
+          <label htmlFor="promo" className="font-semibold inline-block mb-3 text-sm uppercase">Code promo</label>
+          <input type="text" id="promo" placeholder="Enter your code" className="p-2 text-sm w-full"></input>
+        </div>
+        <button className="bg-red-500 hover:bg-red-600 px-5 py-2 text-sm text-white uppercase">Appliquer</button>
+        <div className="border-t mt-8">
+          <div className="flex font-semibold justify-between py-6 text-sm uppercase">
+            <span>Total</span>
+            <span>$600</span>
+          </div>
+          <button className="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full">Payer ma commande</button>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  )
+}
+
+export default Cart
