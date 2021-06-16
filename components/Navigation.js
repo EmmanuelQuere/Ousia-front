a56a2b6488b5303fa3db3051526c46262b7bdb48
Ariane <pikabooariane@hotmail.fr>
2021-06-16 16:42:52 +0300

    added cart without quantity methods

--- components/Navigation.js
+++ components/Navigation.js
@@ -2,6 +2,8 @@ import DisconnectButton from './DisconnectButton';
 import Link from 'next/link';
 import { useSelector } from 'react-redux';
 import { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingBasket} from "@fortawesome/free-solid-svg-icons";
 
 const Navigation = () => {
 
@@ -11,12 +13,13 @@ const Navigation = () => {
   useEffect(() => {
     if (isLoggedIn) {
       setNavbarElements (
-      <nav className="sticky top-0 z-10 bg-white backdrop-filter backdrop-blur-lg bg-opacity-30 border-b border-gray-200 firefox:bg-opacity-20">
+      <nav className="sticky top-0 z-10 bg-white backdrop-filter backdrop-blur-lg bg-opacity-80 border-b border-gray-200 firefox:bg-opacity-20">
         <div className="max-w-5xl mx-auto px-4">
           <div className="flex items-center justify-between h-16">
             <span className="text-2xl text-gray-900 font-semibold">Ousia</span>
             <div className="flex space-x-4 text-gray-900">
               <Link href="/shop"><a>Magasin</a></Link>
+              <Link href="/cart" passHref><a><FontAwesomeIcon icon={faShoppingBasket} /></a></Link>
               <a href="#">La marque</a>
               <a href="#">La Team</a>
             </div>
@@ -32,12 +35,13 @@ const Navigation = () => {
       )
     } else {
       setNavbarElements (
-      <nav className="sticky top-0 z-10 bg-white backdrop-filter backdrop-blur-lg bg-opacity-30 border-b border-gray-200 firefox:bg-opacity-20">
+      <nav className="sticky top-0 z-10 bg-white backdrop-filter backdrop-blur-lg bg-opacity-80 border-b border-gray-200 firefox:bg-opacity-20">
         <div className="max-w-5xl mx-auto px-4">
           <div className="flex items-center justify-between h-16">
             <span className="text-2xl text-gray-900 font-semibold">Ousia</span>
             <div className="flex space-x-4 text-gray-900">
               <Link href="/shop"><a>Magasin</a></Link>
+              <Link href="/cart" passHref><a><FontAwesomeIcon icon={faShoppingBasket} /></a></Link>
               <a href="#">La marque</a>
               <a href="#">La Team</a>
             </div>
--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import CartProduct from '../../components/CartProduct';
+import Link from 'next/link';
 
 // export const getServerSideProps = async (context) => {
 //   const res = await fetch(`${process.env.url}/items.json`);
@@ -11,7 +12,8 @@ import CartProduct from '../../components/CartProduct';
 // }
 
 const Cart = () => {
-  const [currentUserCart, setCurrentUserCart] = React.useState();
+  const [currentUserCart, setCurrentUserCart] = React.useState(null);
+  const [deliveryPrice, setDeliveryPrice] = React.useState(10);
   const userToken = useSelector(state => state.token);
 
   React.useEffect(
@@ -27,12 +29,25 @@ const Cart = () => {
 
       fetch(`${process.env.url}/mycart`, requestOptions)
         .then(response => response.json())
-        .then(response => console.log(response))
-      .then(result=> setCurrentUserCart(result))
-      .catch(error => console.log('error', error));}
+        .then(result => {
+          setCurrentUserCart(result)
+        })
+        .catch(error => console.log('error', error));}
     }
     , []
   )
+
+  const totalPrice = (cart) => {
+    return Object.values(cart).reduce((t, { total }) => t + total, 0)
+  }
+
+  const totalQuantity = (cart) => {
+    return Object.values(cart).reduce((t, { quantity }) => t + quantity, 0)
+  }
+
+  const getSelectValue = (event) => {
+    setDeliveryPrice(Number(event.target.value))
+  }
   
   return (
   <div className="container mx-auto mt-10">
@@ -40,7 +55,7 @@ const Cart = () => {
       <div className="w-3/4 bg-white px-10 py-10">
         <div className="flex justify-between border-b pb-8">
           <h1 className="font-semibold text-2xl">Mon panier</h1>
-          <h2 className="font-semibold text-2xl">3 Items</h2>
+          {!currentUserCart || <h2 className="font-semibold text-2xl">{totalQuantity(currentUserCart)} {totalQuantity(currentUserCart) > 1 ? `articles` : `article`}</h2>}
         </div>
         <div className="flex mt-10 mb-5">
           <h3 className="font-semibold text-gray-600 text-xs uppercase w-2/5">Produit</h3>
@@ -48,39 +63,36 @@ const Cart = () => {
           <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Prix</h3>
           <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 text-center">Total</h3>
           </div>
-          {currentUserCart ? <span>"hello"</span> : <span>Pas de cart ici</span>}
-          <a href="#" className="flex font-semibold text-indigo-600 text-sm mt-10">
-      
-          <svg className="fill-current mr-2 text-indigo-600 w-4" viewBox="0 0 448 512"><path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z"/></svg>
-          Retour à la boutique
-        </a>
+          {!currentUserCart ? <span className="text-gray-600 text-lg">Vous n'avez pas encore d'articles dans votre panier !</span> : currentUserCart.map(cartItem => <CartProduct key={cartItem.id} product={cartItem.item} quantity={cartItem.quantity}/>)}
+          <Link href="/shop">
+            <a className="flex font-semibold text-indigo-600 text-sm mt-10">
+              <svg className="fill-current mr-2 text-indigo-600 w-4" viewBox="0 0 448 512"><path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z" /></svg>
+              Retour à la boutique
+            </a>
+          </Link>
       </div>
 
-      <div id="summary" className="w-1/4 px-8 py-10">
-        <h1 className="font-semibold text-2xl border-b pb-8">Récapitulatif de la commande</h1>
-        <div className="flex justify-between mt-10 mb-5">
-          <span className="font-semibold text-sm uppercase">3 articles</span>
-          <span className="font-semibold text-sm">590$</span>
-        </div>
-        <div>
-          <label className="font-medium inline-block mb-3 text-sm uppercase">Livraison</label>
-          <select className="block p-2 text-gray-600 w-full text-sm">
-            <option>Livraison standard - 10.00€</option>
-          </select>
-        </div>
-        <div className="py-10">
-          <label htmlFor="promo" className="font-semibold inline-block mb-3 text-sm uppercase">Code promo</label>
-          <input type="text" id="promo" placeholder="Enter your code" className="p-2 text-sm w-full"></input>
-        </div>
-        <button className="bg-red-500 hover:bg-red-600 px-5 py-2 text-sm text-white uppercase">Appliquer</button>
-        <div className="border-t mt-8">
-          <div className="flex font-semibold justify-between py-6 text-sm uppercase">
-            <span>Total</span>
-            <span>$600</span>
+        {!currentUserCart || <div id="summary" className="w-1/4 px-8 py-10">
+          <h1 className="font-semibold text-2xl border-b pb-8">Récapitulatif de la commande</h1>
+          <div className="flex justify-between mt-10 mb-5">
+            <span className="font-semibold text-sm uppercase">{totalQuantity(currentUserCart)} {totalQuantity(currentUserCart) > 1 ? `articles` : `article`}</span>
+            <span className="font-semibold text-sm">{totalPrice(currentUserCart)} €</span>
           </div>
-          <button className="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full">Payer ma commande</button>
-        </div>
-      </div>
+          <div>
+            <label className="font-medium inline-block mb-3 text-sm uppercase">Livraison</label>
+            <select className="block p-2 text-gray-600 w-full text-sm" onChange={getSelectValue}>
+              <option value="10">Livraison standard - 10.00€</option>
+              <option value="20">Livraison express - 20.00€</option>
+            </select>
+          </div>
+          <div className="border-t mt-8">
+            <div className="flex font-semibold justify-between py-6 text-sm uppercase">
+              <span>Total</span>
+              <span>{totalPrice(currentUserCart) + deliveryPrice} €</span>
+            </div>
+            <button className="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full">Payer ma commande</button>
+          </div>
+        </div>}
 
     </div>
   </div>
