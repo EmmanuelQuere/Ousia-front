cef1fb8ff4f8a03d4bb8fe4606c2395497ded3c9
Ariane <pikabooariane@hotmail.fr>
2021-06-17 19:04:35 +0300

    feature/stripe added stripe payment and order confirmation to cart

--- components/CheckoutButton.js
+++
@@ -1,7 +0,0 @@
-const CheckoutButton = ({ handleClick }) => (
-  <button type="button" id="checkout-button" role="link" onClick={handleClick}>
-    Payer ma commande
-  </button>
-);
-
-export default CheckoutButton
--- pages/cart/recap.js
+++ pages/cart/recap.js
@@ -1,20 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { loadStripe } from "@stripe/stripe-js";
-import CheckoutMessage from "./CheckoutMessage";
 import { useSelector } from "react-redux";
-import CheckoutButton from "./CheckoutButton";
+import Router from 'next/router';
+import CheckoutRecap from "../../components/CheckoutRecap";
+import CheckoutMessage from "../../components/CheckoutMessage";
 
 const stripePromise = loadStripe("pk_test_51ILoi9D5i13aYcbUguAzFyWWFijcBAUTSwKs82B6piA6SUdImu8A94Ys7TKbYICjB98l0Ev228dHHSvpPciTtR0F00oqJphB5S");
 
-export default function StripeLink(props) {
-  const [title, setTitle] = useState("");
-  const [message, setMessage] = useState("");
+const Recap = () => {
+  const [title, setTitle] = useState(null);
+  const [message, setMessage] = useState(null);
+  const [cartID, setCartID] = useState("");
+  const [cartDetails, setCartDetails] = useState([]);
   const userToken = useSelector(state => state.token);
 
   useEffect(() => {
     const query = new URLSearchParams(window.location.search);
-
-    if (query.get("success")) {
+    if (!userToken) { Router.push("/login") }
+    else if (query.get("success")) {
       setTitle("Merci d'avoir commandé chez Ousia ! 🚀")
       setMessage("Votre commande a été passée avec succès ! Vous allez recevoir un e-mail de confirmation.");
 
@@ -27,22 +30,34 @@ export default function StripeLink(props) {
         headers: myHeaders
       };
 
-      fetch(`${process.env.url}/orders.json?session_id=${query.get("session_id")}&cart_id=${query.get("cart_id")}`, requestOptions)
-        .then(response => response.json())
-        .then(response => console.log(response));
+      fetch(`${process.env.url}/orders.json?session_id=${query.get("session_id")}&cart_id=${query.get("cart_id")}`, requestOptions);
     }
-
-    if (query.get("canceled")) {
+    else if (query.get("canceled")) {
       setTitle("Oups... 🤔")
       setMessage(
         "Votre commande n'a pas pu aboutir. En cas de problème, contactez notre équipe à ventes@ousia.com"
       );
     }
+    if (!query.get("success") && !query.get("cancel")) {
+      let myHeaders = new Headers();
+      myHeaders.append("Authorization", `${userToken}`);
+
+      let requestOptions = {
+        method: 'GET',
+        headers: myHeaders
+      };
+
+      fetch(`${process.env.url}/mycart`, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+          setCartDetails(result);
+          setCartID(result[0].cart_id)
+        })
+        .catch(error => console.log('error', error));
+    }
   }, []);
 
   const handleClick = async (event) => {
-    if (!userToken) { window.location = "/login" }
-
     const stripe = await stripePromise;
 
     let myHeaders = new Headers();
@@ -53,7 +68,7 @@ export default function StripeLink(props) {
       headers: myHeaders
     };
 
-    const response = await fetch(`${process.env.url}/create-checkout-session/${props.cart_id}`, requestOptions);
+    const response = await fetch(`${process.env.url}/create-checkout-session/${cartID}`, requestOptions);
 
     const session = await response.json();
 
@@ -66,9 +81,11 @@ export default function StripeLink(props) {
     }
   };
 
-  return message ? (
-    <CheckoutMessage message={message} title={title} />
-  ) : (
-    <CheckoutButton handleClick={handleClick} />
-  );
-}
+  return (
+    <>
+      {message ? <CheckoutMessage message={message} title={title} /> : <CheckoutRecap handleClick={handleClick} cartDetails={cartDetails} />}
+    </>
+  )
+};
+
+export default Recap
