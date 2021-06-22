8619483ab9ca9df7dfd6ec4ced9b6e770959278f
Caro407 <caro.lefevre407@hotmail.fr>
2021-06-22 16:37:11 +0200

    FEATURE add embed component and update front blog page

--- components/blog/ArticlePreview.js
+++ components/blog/ArticlePreview.js
@@ -5,13 +5,22 @@ const ArticlePreview = (props) => {
 
   return (
 
-    <div className="flex flex-col items-center justify-center m-3 p-3 shadow-md rounded-lg">
-      <div className="block">
-        <Image src={props.article.data.featured_image.url || "/ousiabottles.png"} alt="truc" width={300} height={400} />
-      </div>
-      <div className="">
-        <Link href={`/blog/${props.article.uid}`}><a className="text-lg font-bold text-center">{props.article.data.title[0].text}</a></Link>
-      </div>
+    <div className="mx-2 my-3 shadow-md rounded-b-lg h-full">
+      <Link href={`/blog/${props.article.uid}`}>
+        <div className="cursor-pointer">
+          <div className="w-full">
+            <Image src={props.article.data.featured_image.url || "/jumbotron.jpg"} alt="truc" className="rounded-t" width={500} height={500} />
+          </div>
+          <div className="p-3">
+            <a className="text-lg text-center">{props.article.data.title[0].text}</a>
+            {props.article.tags.map(tag =>
+              <div className="w-1/4 my-1">
+                <p className="border rounded-lg p-1 text-center text-sm">{`#${tag}`}</p>
+              </div>
+            )}
+          </div>
+        </div>
+      </Link>
     </div>
   )
 }
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,6 +1,7 @@
 import Prismic from '@prismicio/client';
 import Link from 'next/link';
-import ArticlePreview from './../../components/blog/ArticlePreview'
+import styles from './../../styles/Blog.module.scss';
+import ArticlePreview from './../../components/blog/ArticlePreview';
 
 const apiEndpoint = 'https://testprismicwithnext407.cdn.prismic.io/api/v2';
 const options = {};
@@ -19,9 +20,9 @@ export async function getStaticProps() {
 const Blog = ({ res }) => {
   console.log(res);
   return (
-    <div>
+    <div className={`${styles.container} h-full`}>
       <h1 className="text-5xl text-center my-3">BLOG</h1>
-      <div className="grid grid-cols-2">
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4">
         {res.map(article =>
           <ArticlePreview article={article} key={article.id}/>
         )}
