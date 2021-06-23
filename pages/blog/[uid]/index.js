2850ceab2e94fd1f53cebe30ecf70bc1c2403775
Caro407 <caro.lefevre407@hotmail.fr>
2021-06-23 09:46:50 +0200

    FIX finalizing padding and margin on blog articles

--- pages/blog/[uid]/index.js
+++ pages/blog/[uid]/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Link from 'next/link';
 import Prismic  from '@prismicio/client';
 import { RichText } from 'prismic-reactjs';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import styles from './../../../styles/Blog.module.scss';
 import BlogBreadcrumbs from './../../../components/blog/BlogBreadcrumbs';
 import BlogTextComponent from './../../../components/blog/BlogTextComponent'
@@ -48,7 +50,6 @@ const Article = ({ res }) => {
     }
   };
 
-  console.log(res);
   const components = {
     text: BlogTextComponent,
     image_with_caption: BlogImageComponent,
@@ -58,22 +59,25 @@ const Article = ({ res }) => {
 
   let component = res.data.body.map( (element, index)=> {
     let BlogComponent = determineElement(element.slice_type)
-    return {component: BlogComponent, props: element}
+    return {component: BlogComponent, props: {...element, key: `blog-component-${index}`} }
   })
 
-  console.log(component);
   return (
     <div>
       <BlogBreadcrumbs article={res.data.title}/>
-      <div className={styles.heading}>
-        <RichText render={res.data.title} />
-      </div>
-      <div className="w-48 lg:w-1/3 border-b-4 border-red-700 h-1 mx-auto mb-4"></div>
       <div className={styles.container}>
+        <div className={styles.heading}>
+          <RichText render={res.data.title} />
+        </div>
+        <div className="w-48 lg:w-1/3 border-b-4 border-red-700 h-1 mx-auto my-8"></div>
         {component.map((data, index) => data.component ? React.createElement(data.component, data.props) : <p></p>)}
       </div>
-      <div className="w-48 lg:w-1/3 border-b-4 border-red-700 h-1 mx-auto mb-4"></div>
-      <Link href="/blog"><a className="m-4 hover:underline">Revenir à la liste des articles</a></Link>
+      <div className="w-48 lg:w-1/3 border-b-4 border-red-700 h-1 mx-auto my-4"></div>
+      <Link href="/blog">
+        <a className="m-4 hover:underline px-12">
+          <FontAwesomeIcon icon={faArrowLeft} className="text-sm mr-2"/>Revenir à la liste des articles
+        </a>
+      </Link>
     </div>
   )
 }
